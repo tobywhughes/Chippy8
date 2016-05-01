82b11a110c635cb7b22c041a72da0fc3a8b5e7e8
Toby Hughes <tobywhughes@gmail.com>
2016-05-01 02:44:11 -0700

    There is life! The game actually runs. Now I just need to finish writing all of the opcodes and try to make things less glitchy. Not in the game of optimization (hence this being written in python), I'm just trying to make both the emulator and code clean and accurate

--- chip8.py
+++ chip8.py
@@ -1,4 +1,5 @@
 from array import array
+from random import randint, seed
 
 class Chip8:
 
@@ -22,6 +23,7 @@ class Chip8:
 	    0xF0, 0x80, 0xF0, 0x80, 0x80])
 
 	def initialize(self, pixels):
+		seed()
 		self.stack = []
 		self.opcode = 0
 		self.memory = [0] * 4096
@@ -29,12 +31,16 @@ class Chip8:
 		self.I = 0
 		self.sp = 0
 		self.V = [0] * 16
+		self.keys = [0] * 16
 		#Init clear screen
 		self.graphics = [0] * pixels
 
 		#Fontset
 		for i in range(80):
 			self.memory[i] = self.fontset[i]
+		
+		self.delay_timer = 0
+		self.sound_timer = 0		
 
 		self.draw_flag = True
 
@@ -47,54 +53,107 @@ class Chip8:
 	def emulate_cycle(self):
 		#Gets opcode
 		self.opcode = self.memory[self.pc] << 8 | self.memory[self.pc + 1]
-		print(hex(self.opcode))
-		print(hex(self.pc))
+		#print(hex(self.opcode))
+		#print(hex(self.pc))
 		#Decodes opcode
+		vx = (self.opcode & 0x0F00) >> 8
+		vy = (self.opcode & 0x00F0) >> 4
+
+
+		#0---
+		if(self.opcode & 0xF000 == 0x0000):
+			#OOEE
+			if(self.opcode == 0x00EE):
+				self.pc = self.stack.pop()	
+			
+			#Not found:
+			else:
+				self.pc -= 2
 		#1NNN
-		if(self.opcode & 0xF000 == 0x1000):
-			self.pc = self.opcode & 0x0FFF
+		elif(self.opcode & 0xF000 == 0x1000):
+			self.pc = (self.opcode & 0x0FFF) - 2
 
 		#2NNN
 		elif(self.opcode & 0xF000 == 0x2000):
 			self.stack.append(self.pc)
-			self.pc = self.opcode & 0x0FFF
+			self.pc = (self.opcode & 0x0FFF) - 2
 
 		#3XNN
 		elif(self.opcode & 0xF000 == 0x3000):
-			x = self.opcode & 0x0F00 >> 8
-			if self.V[x] == self.opcode & 0x00FF:
+			if self.V[vx] == self.opcode & 0x00FF:
 				self.pc += 2
-			self.pc += 2
 		
 		#4XNN
 		elif(self.opcode & 0xF000 == 0x4000):
-			x = self.opcode & 0x0F00 >> 8
-			if self.V[x] != self.opcode & 0x00FF:
+			if self.V[vx] != self.opcode & 0x00FF:
 				self.pc += 2
-			self.pc += 2
 
 		#6XNN
 		elif(self.opcode & 0xF000 == 0x6000):
-			x = self.opcode & 0x0F00 >> 8
-			self.V[x] = self.opcode & 0x00FF
-			self.pc += 2
+			self.V[vx] = self.opcode & 0x00FF
 		
 		#7XNN
 		elif(self.opcode & 0xF000 == 0x7000):
-			x = self.opcode & 0x0F00 >> 8
-			self.V[x] += self.opcode & 0x00FF
-			print(self.V[x])
-			self.pc += 2
+			self.V[vx] += self.opcode & 0x00FF
+
+		#8---
+		elif(self.opcode & 0xF000 == 0x8000):
+			l = self.opcode & 0x000F
+			#8XY0
+			if(l == 0x0000):
+				self.V[vx] = self.V[vy]
+			#8XY1
+			elif(l == 0x0001):
+				self.V[vx] = self.V[vx] | self.V[vy]
+			#8XY2
+			elif(l == 0x0002):
+				self.V[vx] = self.V[vx] & self.V[vy]				
+
+			#8XY3
+			elif(l == 0x0003):
+				self.V[vx] = self.V[vx] ^ self.V[vy]			
+
+			#8XY4
+			elif(l == 0x0004):
+				self.V[vx] += self.V[vy]
+				if self.V[vx] > 0xFF:
+					self.V[0xF] = 1
+				else:
+					self.V[0xF] = 0
+				self.V[vx] = self.V[vx] & 0xFF
+
+			#8XY5
+			elif(l == 0x0005):
+				if self.V[vx] < self.V[vy]:
+					self.V[0xF] = 0
+				else:
+					self.V[0xF] = 1
+				self.V[vx] -= self.V[vy]
+				self.V[vx] = self.V[vx] & 0xFF
+
+
+			else:
+				#Not found
+				self.pc -= 2
+
+		#9XY0
+		elif(self.opcode & 0xF000 == 0x9000):
+			if self.V[vx] != self.V[vy]:
+				self.pc += 2
 
 		#ANNN
 		elif(self.opcode & 0xF000 == 0xA000):
 			self.I = self.opcode & 0x0FFF
-			self.pc += 2
+		
+		#CXNN
+		elif(self.opcode & 0xF000 == 0xC000):
+			rand_int = randint(0, 0xFF)
+			self.V[vx] = rand_int & (self.opcode & 0x00FF)
 
 		#DXYN
 		elif(self.opcode & 0xF000 == 0xD000):
-			xcord = self.V[self.opcode & 0x0F00 >> 8] 
-			ycord = self.V[self.opcode & 0x00F0 >> 4]
+			xcord = self.V[vx] 
+			ycord = self.V[vy]
 			height = self.opcode & 0x000F
 			pixel = 0
 			self.V[0xF] = 0
@@ -108,13 +167,75 @@ class Chip8:
 							self.V[0xF] = 1
 						self.graphics[i] ^= 1
 
-			self.pc += 2
 			self.draw_flag = True
 
-		
+		#E---
+		elif(self.opcode & 0xF000 == 0xE000):
+			#EX9E
+			if (self.opcode & 0x00FF == 0x009E):
+				if self.keys[self.V[vx] == 1]:
+					self.pc += 2
+
+			#EXA1
+			elif (self.opcode & 0x00FF == 0x00A1):
+				if self.keys[self.V[vx]] == 0:
+					self.pc += 2
+
+			#Not found
+			else:
+				self.pc -= 2
+			
+
+		#F---
+		elif(self.opcode & 0xF000 == 0xF000):
+			nn = self.opcode & 0x00FF
+
+			#FX07
+			if(nn == 0x0007):
+				self.V[vx] = self.delay_timer
+
+			#FX15
+			elif(nn == 0x0015):
+				self.delay_timer = self.V[vx]
+
+			#FX18
+			elif(nn == 0x0018):
+				self.sound_timer = self.V[vx]			
+
+			#FX29
+			elif(nn == 0x0029):
+				self.I = self.V[vx] * 5			
+
+			#FX33
+			elif(nn == 0x0033):
+				self.memory[self.I] = self.V[vx] // 100
+				self.memory[self.I + 1] = (self.V[vx] // 10) % 10
+				self.memory[self.I + 2] = (self.V[vx] % 100) % 10
+			
+			#FX65
+			elif(nn == 0x0065):
+				for n in range(vx):
+					self.V[n] = self.memory[self.I + n]			
+
+			#Not found
+			else:
+				self.pc -= 2
+
+
+	
 		#Not currently made print
 		else:
 			print(hex(self.opcode))
+			#saves for debuging purposes
+			self.pc -= 2
+
+		self.pc += 2
+		
+		if self.delay_timer > 0:
+			self.delay_timer -= 1
+
+		if self.sound_timer > 0:
+			self.sound_timer -= 1
 
 	def set_keys(self):
 		pass
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import sys, pygame, chip8
-from pygame import gfxdraw
+from pygame import gfxdraw, Rect
 pygame.init()
 
 #Variables
 def main_func():
+	keys=[pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4, 
+              pygame.K_q, pygame.K_w, pygame.K_e, pygame.K_r,
+              pygame.K_a, pygame.K_s, pygame.K_d, pygame.K_f,
+	      pygame.K_z, pygame.K_x, pygame.K_c, pygame.K_v]
 	my_chip8 = chip8.Chip8()
 	black = 0,0,0
 	white = 255,255,255
 	colors = [black, white]
-	size = width, height = 64 , 32
+	width, height = 64 , 32
+	size = width * 10, height * 10
 	pixels = width * height
 	
 	
@@ -16,7 +21,6 @@ def main_func():
 
 	#Set up emulation system
 	setup_graphics(screen)
-	setup_input()
 
 	#Initialize chip8 system
 	my_chip8.initialize(pixels)
@@ -32,18 +36,30 @@ def main_func():
 		if(my_chip8.draw_flag):
 			draw_graphics(screen, colors, my_chip8, width, height)
 		
-		my_chip8.set_keys()
+		events = pygame.event.get()	
+		get_key_event(events, keys, my_chip8)
 
 def setup_graphics(screen):
 	screen.fill((0,0,0))
 
-def setup_input():
-	pass
+def get_key_event(events, keys, my_chip8):
+	for event in events:
+		event_type = -1
+		if event.type == pygame.KEYDOWN:
+			event_type = 1	
+		elif event.type == pygame.KEYUP:
+			event_type = 0
+		if event_type != -1:
+			print(event.key)
+			if event.key in keys:
+				i = keys.index(event.key)
+				my_chip8.keys[i] = event_type
+		 
 
 def draw_graphics(screen, colors, my_chip8, width, height):
 	for x in range(width):
 		for y in range(height):
-			gfxdraw.pixel(screen, x, height - y, colors[my_chip8.graphics[x + (y * width)]])
+			screen.fill(colors[my_chip8.graphics[x + (y * width)]], Rect(x*10, y*10, 10, 10))
 
 	pygame.display.flip()
 	my_chip8.draw_flag = False
